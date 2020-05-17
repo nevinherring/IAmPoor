1e7512765139764c6c3687b8657eb995c44bed0c
Nevin Herring <nevin.herring@gmail.com>
2020-05-17 11:18:43 -0500

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Nevin Herring on 5/17/20.
+//  Copyright © 2020 Nevin Herring. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
